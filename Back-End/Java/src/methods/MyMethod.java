d45c847b20dea0dd81c22bc21b64e776c7e53363
Tati-Ramos <tatiane_sr@outlook.com>
2021-08-25 11:43:39 -0300

    Commit

---
+++ Back-End/Java/src/methods/MyMethod.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class MyMethod {
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
+
