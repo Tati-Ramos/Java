7a259ea3773f213b33a88a408c00d64c0cfc0d2b
Tati-Ramos <tatiane_sr@outlook.com>
2021-10-06 11:11:43 -0300

    Commit

--- Java/src/filehandling/DeleteFile.java
+++ Java/src/filehandling/DeleteFile.java
@@ -3,13 +3,13 @@ package filehandling;
 import java.io.File;  // Import the File class
 
 public class DeleteFile {
-    public static void main(String[] args) {
-        File myObj = new File("filename.txt");
-        if (myObj.delete()) {
-            System.out.println("Deleted the file: " + myObj.getName());
-        } else {
-            System.out.println("Failed to delete the file.");
-        }
+//    public static void main(String[] args) {
+//        File myObj = new File("filename.txt");
+//        if (myObj.delete()) {
+//            System.out.println("Deleted the file: " + myObj.getName());
+//        } else {
+//            System.out.println("Failed to delete the file.");
+//        }
     }
 }
 
@@ -17,4 +17,9 @@ public class DeleteFile {
 /*
 Delete a File
 To delete a file in Java, use the delete() method
+ */
+
+
+/*
+
  */
---
+++ Java/src/filehandling/DeleteFolder.java
@@ -0,0 +1,20 @@
+package filehandling;
+
+import java.io.File;
+
+public class DeleteFolder {
+//    public static void main(String[] args) {
+//        File myObj = new File("C:\\Users\\tatia\\Documents\\Learning\\Java");
+//        if (myObj.delete()) {
+//            System.out.println("Deleted the folder: " + myObj.getName());
+//        } else {
+//            System.out.println("Failed to delete the folder.");
+//        }
+    }
+}
+
+
+/*
+Delete a Folder
+You can also delete a folder. However, it must be empty
+ */
